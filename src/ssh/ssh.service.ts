2a5e84a0edbaa1068fa64c55fd3375e9bb1f6370
NextBrayer <j.brayer@nextronic.io>
2023-10-12 15:48:11 +0100

    ssh fixing

--- src/ssh/ssh.service.ts
+++ src/ssh/ssh.service.ts
@@ -1,7 +1,7 @@
-import * as readline from 'readline';
 import { Injectable, forwardRef, Inject } from '@nestjs/common';
+import { execSync } from 'child_process';
 import { SocketService } from 'src/socket/socket.service';
-import { spawn } from 'child_process';
+
 @Injectable()
 export class SshService {
   constructor(
@@ -11,38 +11,13 @@ export class SshService {
     console.log('command execution init');
   }
 
-  executeCommand() {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-    rl.question('Enter a command (Ctrl+C to exit): ', (command) => {
-      if (command === '') {
-        this.executeCommand(); // Prompt again if no command is entered
-        return;
-      }
-
-      if (command === 'cd ..') {
-        process.chdir('..'); // Change the current working directory
-        console.log(`Changed directory to: ${process.cwd()}`);
-        this.executeCommand(); // Prompt for the next command
-        return;
-      }
-
-      const childProcess = spawn(command, { shell: true });
-
-      childProcess.stdout.on('data', (data) => {
-        console.log(`Command Output: ${data}`);
-      });
-
-      childProcess.stderr.on('data', (data) => {
-        console.error(`Command Error: ${data}`);
-      });
-
-      childProcess.on('close', (code) => {
-        console.log(`Child process exited with code ${code}`);
-        this.executeCommand(); // Prompt for the next command
-      });
-    });
+  executeCommand(data: any) {
+    try {
+      const result = execSync(data).toString();
+      this.socket.send('command_result', result);
+    } catch (error) {
+      console.log('status', error.message);
+      this.socket.send('command_result', error.message);
+    }
   }
 }
