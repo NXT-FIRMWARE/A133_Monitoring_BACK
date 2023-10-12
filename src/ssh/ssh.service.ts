3b079d70c67712c9ee0429f927587c7f50635ecd
NextBrayer <j.brayer@nextronic.io>
2023-10-12 12:27:44 +0100

    ssh fixing

--- src/ssh/ssh.service.ts
+++ src/ssh/ssh.service.ts
@@ -13,19 +13,19 @@ export class SshService {
   }
 
   executeCommand(data: any) {
-    let result = '';
-    if (platform() === 'linux') {
-      try {
-        result = execSync(data).toString();
-        console.log('the reult is :', result);
-        if (result.toString().includes('Error'))
-          this.socket.send('comand_result', result);
-        else {
-          this.socket.send('comand_result', result);
-        }
-      } catch (error) {
-        this.socket.send('comand_result', error);
-      }
+    try {
+      const result = execSync(data).toString();
+      this.socket.send('command_result', result);
+    } catch (error) {
+      error.status; // 0 : successful exit, but here in exception it has to be greater than 0
+      console.log('status', error.status);
+      error.message; // Holds the message you typically want.
+      console.log('status', error.message);
+      error.stderr; // Holds the stderr output. Use `.toString()`.
+      console.log('status', error.stderr.toString());
+      error.stdout; // Holds the stdout output. Use `.toString()`.
+      console.log('status', error.stdout.toString());
+      this.socket.send('command_result', error.message);
     }
   }
 }
