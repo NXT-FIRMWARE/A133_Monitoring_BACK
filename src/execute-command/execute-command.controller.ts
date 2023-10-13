df0e0a5e12959ac5d7820c3841f1a31f9b84c006
NextBrayer <j.brayer@nextronic.io>
2023-10-13 10:23:55 +0100

    delete xecute command controller

--- src/execute-command/execute-command.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { ExecuteCommandService } from './execute-command.service';
-
-@Controller('execute-command')
-export class ExecuteCommandController {
-  constructor(private readonly executeCommandService: ExecuteCommandService) {}
-}
