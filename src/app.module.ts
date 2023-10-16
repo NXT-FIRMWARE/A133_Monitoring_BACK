abe20bed66755d08445c3f8640cd9332e38a21c1
NextBrayer <j.brayer@nextronic.io>
2023-10-16 18:15:27 +0100

    fix cam

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { SocketModule } from './socket/socket.module';
 import { CameraModule } from './camera/camera.module';
 import { DatabaseModule } from './database/database.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [SocketModule, CameraModule, DatabaseModule],
+  imports: [
+    SocketModule,
+    CameraModule,
+    DatabaseModule,
+    ScheduleModule.forRoot(),
+  ],
 })
 export class AppModule {}
