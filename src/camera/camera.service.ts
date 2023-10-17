258fd4fb8e9b727fe9a9503a10c841f06234596e
NextBrayer <j.brayer@nextronic.io>
2023-10-17 12:05:05 +0100

    capture time fixing

--- src/camera/camera.service.ts
+++ src/camera/camera.service.ts
@@ -71,34 +71,26 @@ export class CameraService {
 
   captureProcess() {
     console.log('capturing');
-    setInterval(
-      () => {
-        this.recorder.map((recItem) => {
-          this.capture_time = recItem.capture_time;
-          console.log(
-            'capturing interval',
-            recItem.capture_time,
-            this.capture_time,
-          );
-          const storage = execSync(
-            `df -h ${recItem.recorder.folder} | awk 'NR==2 {print $4}'`,
-          ).toString();
-          const typeKB = storage.includes('K');
-          const sizeValue = +storage.replace(/[GMK]/gi, '');
-          console.log('capturing ', !(typeKB && sizeValue < 150));
-          if (!(typeKB && sizeValue < 150)) {
-            recItem.recorder.captureImage(() => {
-              this.logger.log(
-                'image saved to ',
-                recItem.recorder.folder + recItem.recorder.camera,
-              );
-            });
-          } else {
-            this.logger.log('stop Saving in memory ');
-          }
-        });
-      },
-      this.capture_time === undefined ? 5000 : this.capture_time,
-    );
+
+    this.recorder.map((recItem) => {
+      setInterval(() => {
+        const storage = execSync(
+          `df -h ${recItem.recorder.folder} | awk 'NR==2 {print $4}'`,
+        ).toString();
+        const typeKB = storage.includes('K');
+        const sizeValue = +storage.replace(/[GMK]/gi, '');
+        console.log('capturing ', !(typeKB && sizeValue < 150));
+        if (!(typeKB && sizeValue < 150)) {
+          recItem.recorder.captureImage(() => {
+            this.logger.log(
+              'image saved to ',
+              recItem.recorder.folder + recItem.recorder.camera,
+            );
+          });
+        } else {
+          this.logger.log('stop Saving in memory ');
+        }
+      }, recItem.capture_time);
+    });
   }
 }
