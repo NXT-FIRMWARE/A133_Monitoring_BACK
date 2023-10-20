166005cf3f70dec9a70d0e12518018fbcfc46f1b
NextBrayer <j.brayer@nextronic.io>
2023-10-20 17:54:06 +0100

    add static & dhcp

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SocketModule } from './socket/socket.module';
 import { NetworkModule } from './network/network.module';
 
 @Module({
-  imports: [SocketModule, NetworkModule],
+  imports: [NetworkModule],
   controllers: [AppController],
   providers: [AppService],
 })
