c650279a3bbfc4a6d58a608567f5347734305bc9
NextBrayer <j.brayer@nextronic.io>
2023-10-27 10:35:45 +0100

    add shell

--- src/shell/shell.controller.ts
+++ src/shell/shell.controller.ts
@@ -1,12 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
 import { ShellService } from './shell.service';
-
+import { Response } from 'express';
 @Controller('shell')
 export class ShellController {
   constructor(private readonly shellService: ShellService) {}
 
   @Post()
-  getStatus(@Body() data: any) {
-    return this.shellService.executeCommand(data.command);
+  async getStatus(@Res() res: Response, @Body() data: any) {
+    const result: string = await this.shellService.executeCommand(data.command);
+    if (result.includes('Command failed'))
+      return res.status(HttpStatus.BAD_REQUEST).send(result);
+    else return res.status(HttpStatus.CREATED).send(result);
   }
 }
