c470e51d8ea1f4d2b1329eb0860d00dd621eb627
NextBrayer <j.brayer@nextronic.io>
2023-10-30 15:59:49 +0100

    file sys

--- src/file_system/file_system.service.ts
+++ src/file_system/file_system.service.ts
@@ -3,19 +3,14 @@ import * as fs from 'fs';
 import * as path from 'path';
 @Injectable()
 export class FileSystemService {
-  getFiles(dir: any) {
+  getFiles(dir: string) {
     try {
-      //joining path of directory
       const directoryPath = path.join(__dirname, dir);
-      //passsing directoryPath and callback function
       fs.readdir(directoryPath, function (err, files) {
-        //handling error
         if (err) {
           return console.log('Unable to scan directory: ' + err);
         }
-        //listing all files using forEach
         files.forEach(function (file) {
-          // Do whatever you want to do with the file
           console.log(file);
         });
         return files;
